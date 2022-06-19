e5a67b951c59af94f3a95767c5674aaf753cf855
J-Carlos-M-A <carlitosjl77@gmail.com>
2022-06-19 00:18:08 -0500

    comite

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,173 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <AppBar />
+    <v-main>
+      <div class="d-flex justify-center ma-4">{{ turno }}</div>
+      <div class="d-flex justify-center align-center">
+        <div class="d-flex flex-wrap" style="width: 250px">
+          <v-btn
+            v-for="boton in botones"
+            :key="boton.id"
+            outlined
+            color="indigo"
+            class="ma-2"
+            @click="tira(boton)"
+          >
+            {{ boton.texto }}
+          </v-btn>
+        </div>
+      </div>
+    </v-main>
+
+    <Alert :valor="alertaText" :mostrar="mostrar" />
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppBar from "./components/AppBar.vue";
+import Alert from "./components/Alert.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    //
+    AppBar,
+    Alert,
+  },
+
+  data: () => ({
+    //
+    turno: "X",
+    alertaText: "",
+    mostrar: false,
+    botones: [
+      {
+        id: 1,
+        texto: "",
+      },
+      {
+        id: 2,
+        texto: "",
+      },
+      {
+        id: 3,
+        texto: "",
+      },
+      {
+        id: 4,
+        texto: "",
+      },
+      {
+        id: 5,
+        texto: "",
+      },
+      {
+        id: 6,
+        texto: "",
+      },
+      {
+        id: 7,
+        texto: "",
+      },
+      {
+        id: 8,
+        texto: "",
+      },
+      {
+        id: 9,
+        texto: "",
+      },
+    ],
+  }),
+  methods: {
+    tira(boton) {
+      if (boton.texto == "") {
+        boton.texto = this.turno;
+        this.checkWin(this.turno);
+        if (this.turno == "X") {
+          this.turno = "O";
+        } else {
+          this.turno = "X";
+        }
+      } else {
+        console.log("seas mamon qui ya tiraron");
+      }
+    },
+    win(jugador) {
+      this.mostrar = true;
+      this.alertaText = jugador;
+      setTimeout(() => {
+        this.mostrar = false;
+      }, 2500);
+    },
+    checkWin(variale) {
+      if (
+        this.botones[0].texto == variale &&
+        this.botones[1].texto == variale &&
+        this.botones[2].texto == variale
+      ) {
+        this.win(variale);
+      } else {
+        if (
+          this.botones[3].texto == variale &&
+          this.botones[4].texto == variale &&
+          this.botones[5].texto == variale
+        ) {
+          this.win(variale);
+        } else {
+          if (
+            this.botones[6].texto == variale &&
+            this.botones[7].texto == variale &&
+            this.botones[8].texto == variale
+          ) {
+            this.win(variale);
+          } else {
+            if (
+              this.botones[0].texto == variale &&
+              this.botones[3].texto == variale &&
+              this.botones[6].texto == variale
+            ) {
+              this.win(variale);
+            } else {
+              if (
+                this.botones[1].texto == variale &&
+                this.botones[4].texto == variale &&
+                this.botones[7].texto == variale
+              ) {
+                this.win(variale);
+              } else {
+                if (
+                  this.botones[3].texto == variale &&
+                  this.botones[5].texto == variale &&
+                  this.botones[8].texto == variale
+                ) {
+                  this.win(variale);
+                } else {
+                  if (
+                    this.botones[0].texto == variale &&
+                    this.botones[4].texto == variale &&
+                    this.botones[8].texto == variale
+                  ) {
+                    this.win(variale);
+                  } else {
+                    if (
+                      this.botones[2].texto == variale &&
+                      this.botones[4].texto == variale &&
+                      this.botones[6].texto == variale
+                    ) {
+                      this.win(variale);
+                    }else{
+                      this.win("empate");
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    },
+  },
+};
+</script>
---
+++ src/components/Alert.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-alert v-model="alerta" border="top" type="success" dark>
+      Gana jugador {{ valor }}
+    </v-alert>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    valor: {
+      type: String,
+    },
+    mostrar: {
+      type: Boolean,
+    },
+  },
+  data: () => ({
+    //
+    alerta: false,
+  }),
+  watch: {
+    // whenever question changes, this function will run
+    mostrar(value) {
+      this.alerta = value;
+    },
+  },
+};
+</script>
